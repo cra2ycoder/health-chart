6cdf5397911e7087dee732abafdee9ad402612c8
mohanraj <mohanraj@skava.com>
2021-07-03 23:23:28 +0530

    prettier fix

--- pages/api/nutrition.js
+++ pages/api/nutrition.js
@@ -1,12 +1,14 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs')
+const path = require('path')
 
 export default function handler(req, res) {
+  fs.writeFile(
+    path.resolve('./data/xxx.json'),
+    'Hello content!',
+    function (err) {
+      if (err) throw err
+    }
+  )
 
-  
-  fs.writeFile(path.resolve("./data/xxx.json"), "Hello content!", function (err) {
-    if (err) throw err;
-  });
-
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ name: 'John Doe' })
 }
